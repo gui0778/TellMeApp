c2ae24a71c83e2e3688812105f99bf01887fb647
gui0778 <gui0778@github.com>
2013-12-13 10:49:39 +0800

    add new codertools

---
+++ PublicLib/src/com/tellme/common/util/CoderTools.java
@@ -0,0 +1,19 @@
+package com.tellme.common.util;
+
+import io.netty.buffer.ByteBuf;
+
+public class CoderTools {
+	public static  byte[] getencoderChecksum(ByteBuf buf) {
+		return null;
+		
+	}
+	public static  byte[] getDecoderChecksum(ByteBuf buf) {
+		return null;
+		
+	}
+	public static  byte[] CalculaterChecksum(ByteBuf buf) {
+		return null;
+		
+	}
+
+}
